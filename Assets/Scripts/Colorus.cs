c71332f7f8abf928c910c861d5b239c24a42e3b5
volgogradsky <33860913+volgogradsky@users.noreply.github.com>
2017-11-22 19:25:07 +0300

    sasat+lezhat

---
+++ Assets/Scripts/Colorus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Colorus : MonoBehaviour {
+
+    public GameObject[] avatars = new GameObject[5];
+    int i = 1;
+ 
+
+
+    void Start () {
+      
+    }
+	
+
+	void Update () {
+		
+	}
+}
